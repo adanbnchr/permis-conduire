80d9e735022f24b64b07376c4bbbef489bab3e29
amine <amine_bel_hadj_90@yahoo.com>
2018-06-02 18:05:55 +0100

    add translations files (multi language website)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,19 +4,23 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 use AppBundle\Controller\base\BaseController;
 
 class DefaultController extends BaseController
 {
-    /**
-     * @Route("/", name="homepage")
+     /**
+     * 
+     * @Route( "/", name = "homepage" )
+     * @Method({"GET", "POST"})
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+//        return $this->render('default/index.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+//        ]);
+        return $this->render('AppBundle:Default:index.html.twig', array('lm'=>'home'));
     }
 }
