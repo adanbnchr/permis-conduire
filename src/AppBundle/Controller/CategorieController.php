d1cc745679f3cefce5311d48e2cc977c06cc74bf
amine <amine_bel_hadj_90@yahoo.com>
2018-06-03 15:31:13 +0100

    add general table

---
+++ src/AppBundle/Controller/CategorieController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Controller\base\BaseController;
+use AppBundle\Entity\Categorie;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Categorie controller.
+ *
+ */
+class CategorieController extends BaseController {
+
+    /**
+     * Lists all categorie entities.
+     *
+     */
+    public function indexAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $categories = $em->getRepository('AppBundle:Categorie')->findAll();
+
+        return $this->render('AppBundle:generic:tableau.html.twig', array(
+                    'data' => $categories, 'fields' =>$this->getFields(), 'keys' => $this->getKeys()
+        ));
+    }
+
+    public function getFields() {
+        return [
+                'Libelle',
+                'Description',
+                'Updatedat',
+                'Createdat',
+                'Id',
+                'Actions'
+        ];
+    }
+    
+    public function getKeys() {
+        return [
+                'libelle',
+                'description',
+                'updatedAt',
+                'createdAt',
+                'id'
+        ];
+    }
+
+    /**
+     * Creates a new categorie entity.
+     *
+     */
+    public function newAction(Request $request) {
+        $categorie = new Categorie();
+        $form = $this->createForm('AppBundle\Form\CategorieType', $categorie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($categorie);
+            $em->flush();
+
+            return $this->redirectToRoute('categorie_show', array('id' => $categorie->getId()));
+        }
+
+        return $this->render('AppBundle:categorie:new.html.twig', array(
+                    'categorie' => $categorie,
+                    'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a categorie entity.
+     *
+     */
+    public function showAction(Categorie $categorie) {
+        $deleteForm = $this->createDeleteForm($categorie);
+
+        return $this->render('AppBundle:generic:show.html.twig', array(
+                    'categorie' => $categorie,
+                    'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing categorie entity.
+     *
+     */
+    public function editAction(Request $request, Categorie $categorie) {
+        $deleteForm = $this->createDeleteForm($categorie);
+        $editForm = $this->createForm('AppBundle\Form\CategorieType', $categorie);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('categorie_edit', array('id' => $categorie->getId()));
+        }
+
+        return $this->render('AppBundle:categorie:edit.html.twig', array(
+                    'categorie' => $categorie,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a categorie entity.
+     *
+     */
+    public function deleteAction(Request $request, Categorie $categorie) {
+        $form = $this->createDeleteForm($categorie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($categorie);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('categorie_index');
+    }
+
+    /**
+     * Creates a form to delete a categorie entity.
+     *
+     * @param Categorie $categorie The categorie entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Categorie $categorie) {
+        return $this->createFormBuilder()
+                        ->setAction($this->generateUrl('categorie_delete', array('id' => $categorie->getId())))
+                        ->setMethod('DELETE')
+                        ->getForm()
+        ;
+    }
+
+}
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,25 +2,12 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-
 use AppBundle\Controller\base\BaseController;
 
 class DefaultController extends BaseController
 {
-     /**
-     * 
-     * @Route( "/", name = "homepage" )
-     * @Method({"GET", "POST"})
-     */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-//        return $this->render('default/index.html.twig', [
-//            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-//        ]);
         return $this->render('AppBundle:Default:index.html.twig', array('lm'=>'home'));
     }
 }
